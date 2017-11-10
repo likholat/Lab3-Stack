9ffb99060d2eaaac4656a4cf21bfad4d162eb4a6
ann <aniali201398@gmail.com>
2017-11-11 02:22:03 +0300

    started cpp

--- Stack/Stack.cpp
+++ Stack/Stack.cpp
@@ -1,2 +1,22 @@
 #include "stdafx.h"
 #include "Stack.h"
+
+template <class T> 
+TStack<T>::TStack(int _MaxSize = 100){
+	
+	if(_MaxSize < 0)
+			throw _MaxSize;
+		MaxSize = _MaxSize;
+		Size = 0;
+		Mas = new T[MaxSize];
+}
+
+template <class T> 
+TStack<T>::TStack(const TStack &ts){
+
+	MaxSize = ts.MaxSize;
+		Size = ts.Size;
+		Mas = new T[MaxSize];
+		for (i = 0; i < Size; i++)
+			Mas[i] = ts.Mas[i];
+	}
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -12,4 +12,60 @@ protected:
 	int Size;
 	int MaxSize;
 	T *Mas;
+
+public:
+	TStack (int _MaxSize = 100);      //конструктор
+	~TStack (){ delete []Mas; }       //деструктор
+	TStack (const TStack &ts);        //конструктор копирования
+
+	TStack & operator = (const TStack &ts){       //оператор присваивания
+		
+		if (MaxSize != ts.MaxSize){
+			delete []Mas;
+			MaxSize = ts.MaxSize;
+			Mas = new T[MaxSize];
+		}
+		Size = ts.Size;
+		for (i = 0; i < Size; i++)
+			Mas[i] = ts.Mas[i];
+		return *this;
+	}
+
+	bool IsEmpty(){                               //проверка на пустоту
+		
+		if (Size == 0)
+			return true;
+		return false;
+	}
+
+	bool IsFull(){                               //проверка на заполненность
+	
+		if (Size == MaxSize)
+			return true;
+		return false;
+	}
+
+	void Push (T a){                             //положить элемент
+	
+		if(IsFull())
+			throw "ERROR";
+		Mas[Size] = a;
+		Size++;
+	}
+
+	T Top() const{                               //узнать последний элемент
+
+		if(IsEmpty())
+			throw "EMPTY";
+		return Mas[Size-1];
+	}
+
+	T Pop(){                                    //вытащить последний элемент
+
+		if(IsEmpty())
+			throw "EMPTY";
+		Size--;
+		return Mas[Size];
+	}
 };
+
